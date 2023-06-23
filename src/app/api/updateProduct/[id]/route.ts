29185c17aa31aa6cde9e2d2ee5f73c6696922364
Bruna Santos <brunahta2025@hotmail.com>
2023-06-22 21:03:15 -0300

    dynamic route

---
+++ src/app/api/updateProduct/[id]/route.ts
@@ -0,0 +1,39 @@
+import { PrismaClient } from "@prisma/client";
+import { NextResponse } from "next/server";
+
+const prisma = new PrismaClient();
+
+export async function PUT(request: Request, { params }) {
+    const id = params.id;
+    const { quantity } = await request.json();
+
+    try {
+        const productId = Number(id);
+
+        const product = await prisma.products.findUnique({
+            where: { id: Number(productId) },
+        });
+        if (!product) {
+            return new NextResponse("Product unique not found", {
+                status: 404,
+            });
+        }
+        const updateProduct = await prisma.products.update({
+            where: { id: Number(productId) },
+            data: { quantity: Number(quantity) },
+        });
+        if (!updateProduct) {
+            return new NextResponse("Product not found", { status: 404 });
+        }
+        const json = JSON.stringify(updateProduct);
+        return new NextResponse(json, {
+            status: 200,
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+    } catch (error) {
+        console.error(error);
+        return new NextResponse("Failed to update product", { status: 500 });
+    }
+}
