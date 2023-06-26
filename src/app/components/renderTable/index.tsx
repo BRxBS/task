de315bd20e65e2d645789a34b2fc74d24a7ed6fd
Bruna Santos <brunahta2025@hotmail.com>
2023-06-26 16:43:12 -0300

    get

--- src/app/components/renderTable/index.tsx
+++ src/app/components/renderTable/index.tsx
@@ -5,7 +5,7 @@ import axios from "axios";
 export async function fetchProducts() {
     try {
         const response = await axios.get(
-            "http://localhost:3000/api/productGet"
+            "https://task-brxbs.vercel.app/api/productGet"
         );
         return response.data;
     } catch (error) {
